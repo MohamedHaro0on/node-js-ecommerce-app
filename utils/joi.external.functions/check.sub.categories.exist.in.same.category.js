ac67aeba0d0e4a9814d4cb1105063570941dd6e8
MohamedHaro0on <mohamedharoon286@gmail.com>
2025-01-12 17:36:18 +0200

    Making the Error Message More user Friendly

--- utils/joi.external.functions/check.sub.categories.exist.in.same.category.js
+++ utils/joi.external.functions/check.sub.categories.exist.in.same.category.js
@@ -2,24 +2,25 @@ import mongoose from "mongoose";
 import SubCategoryModel from "../../modules/subcategory/model/subCategory.model.js";
 import ApiError from "../api.error.js";
 import { StatusCodes } from "http-status-codes";
+import CategoryModel from "../../modules/category/model/category.model.js";
 
 const CheckSubCategoryExistInTheSameCategory = async (
   subCategoryId,
-  categoryId
+  category
 ) => {
   try {
-    const categoryObjectId = new mongoose.Types.ObjectId(categoryId);
-    const subCategoryObjectId = new mongoose.Types.ObjectId(subCategoryId);
-
+    const subCategory = await SubCategoryModel.findById(subCategoryId);
     // Check if the subCategory exists and belongs to the category
-    const subCategory = await SubCategoryModel.findOne({
-      _id: subCategoryObjectId,
-      mainCategoryId: categoryObjectId,
-    });
+    const subCategoryExistsInTheParentCategory = await SubCategoryModel.findOne(
+      {
+        _id: subCategory._id,
+        mainCategoryId: category._id,
+      }
+    );
 
-    if (!subCategory) {
+    if (!subCategoryExistsInTheParentCategory) {
       throw new ApiError(
-        `SubCategory with ID ${subCategoryId} not found or does not belong to the specified category ${categoryId}`,
+        `SubCategory with name " ${subCategory.name} "  not found or does not belong to the specified category " ${category.name} "`,
         StatusCodes.BAD_REQUEST
       );
     }
@@ -36,10 +37,11 @@ const CheckSubCategoriesExistInTheSameCategory = async (
 ) => {
   try {
     const categoryId = helper.state.ancestors[0].category;
+    const category = await CategoryModel.findById(categoryId);
 
     // Validate each subCategoryId
     for (const subCategoryId of subCategoryIds) {
-      await CheckSubCategoryExistInTheSameCategory(subCategoryId, categoryId);
+      await CheckSubCategoryExistInTheSameCategory(subCategoryId, category);
     }
 
     return subCategoryIds; // Return the valid subCategoryIds
